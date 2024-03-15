16f61a42c85cb1acff08ec97e4b987945d45d4fa
肝帝熙恩 <111548550+THEXN@users.noreply.github.com>
2024-03-15 15:03:05 +0800

    Update README.md

--- LifemaxExtra/Configuration.cs
+++ LifemaxExtra/Configuration.cs
@@ -9,9 +9,9 @@ namespace LifemaxExtra
     public class Configuration
     {
         public static readonly string FilePath = Path.Combine(TShock.SavePath, "LifemaxExtra.json");
-        [JsonProperty("使用生命水晶最高可提升至")]
+
         public int LifeCrystalMaxLife = 400;
-        [JsonProperty("使用生命果最高可提升至")]
+
         public int LifeFruitMaxLife = 500;
 
         public void Write(string path)
